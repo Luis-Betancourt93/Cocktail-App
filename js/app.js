0e6388025c602b7ab89638a2cf855b90fcfa0607
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-09-06 09:03:11 -0700

    Carousel Complete
    Got the carousel to work, made a variable then made a function that will increase and decrease it. Made that variable the placeholder of the array index.

--- js/app.js
+++ js/app.js
@@ -2,47 +2,40 @@
 // The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM.
 
 document.querySelector('button').addEventListener('click', getDrink);
+document.getElementById('perv').addEventListener('click', pervDrink);
+document.getElementById('next').addEventListener('click', nextDrink);
+
+
+let i = 0;
+
+function pervDrink() {
+  getDrink();
+  i --;
+  console.log(i);
+}
+    
+function nextDrink() {
+  getDrink();
+  i ++;
+  console.log(i);    
+}
+
 
 function getDrink() {
   let drink = document.querySelector('input').value
-  
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
   .then(res => res.json())
   .then(data => {
-     document.getElementById('drinkName').innerHTML = data.drinks[0].strDrink;
-     document.getElementById('cocktail').src = data.drinks[0].strDrinkThumb;
-     document.getElementById('step').innerHTML = data.drinks[0].strInstructions;
-  
+    document.getElementById('drinkName').innerHTML = data.drinks[i].strDrink;
+    document.getElementById('cocktail').src = data.drinks[i].strDrinkThumb;
+    document.getElementById('step').innerHTML = data.drinks[i].strInstructions;
   })
   .catch(err => {
     console.log(`error ${err}`)
     alert("Please enter a valid cocktail");
   })
-
-
 }
  
-
-
-
-
-
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-// .then(res => res.json())
-// .then(data => {
-//   console.log(data);
-//   console.log(data.drinks);
-//   let myDrink = data.drinks[3];
-//   console.log(myDrink.strDrink);
-  
-//   document.querySelector("h1").innerHTML = myDrink.strDrink;
-//   let ingredients = [myDrink.strIngredient1, myDrink.strIngredient2, myDrink.strIngredient3, myDrink.strIngredient4 ]
-//   document.querySelector("h2").innerHTML = ingredients;
-// })
-// .catch(err => {
-//   console.log(`error ${err}`)
-// })
-
 
 
 
